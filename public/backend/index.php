88ad373b6b8449ddf0f2313def2e35e741d2ff71
Hector Mendoza <hector.mendozam@alumno.buap.mx>
2024-12-01 14:17:27 -0600

    Cambios en la estructura del backend

---
+++ public/backend/index.php
@@ -0,0 +1,13 @@
+<?php
+// Iniciar la sesión
+session_start();
+
+// Verificar si el usuario está autenticado
+if (!isset($_SESSION['user_id'])) {
+    // Si no está autenticado, redirigir al login
+    header("Location: login.html");
+    exit();
+}
+
+include '../index.html'
+?>
