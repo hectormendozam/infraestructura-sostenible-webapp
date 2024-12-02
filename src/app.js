9ecbd779179c4a44ace0d78da49d3c3dcb6eddc7
Hector Mendoza <hector.mendozam@alumno.buap.mx>
2024-12-01 19:30:39 -0600

    Cambios en app.js

--- src/app.js
+++ src/app.js
@@ -52,125 +52,92 @@ $(document).ready(function(){
                 success: function (response) {
                     if(!response.error) {
                         // SE OBTIENE EL OBJETO DE DATOS A PARTIR DE UN STRING JSON
-                        const productos = JSON.parse(response);
+                        const proyectos = JSON.parse(response);
                         
                         // SE VERIFICA SI EL OBJETO JSON TIENE DATOS
-                        if(Object.keys(productos).length > 0) {
+                        if(Object.keys(proyectos).length > 0) {
                             // SE CREA UNA PLANTILLA PARA CREAR LAS FILAS A INSERTAR EN EL DOCUMENTO HTML
                             let template = '';
-                            let template_bar = '';
 
-                            productos.forEach(producto => {
-                                // SE CREA UNA LISTA HTML CON LA DESCRIPCIÓN DEL PRODUCTO
-                                let descripcion = '';
-                                descripcion += '<li>precio: '+producto.precio+'</li>';
-                                descripcion += '<li>unidades: '+producto.unidades+'</li>';
-                                descripcion += '<li>modelo: '+producto.modelo+'</li>';
-                                descripcion += '<li>marca: '+producto.marca+'</li>';
-                                descripcion += '<li>detalles: '+producto.detalles+'</li>';
+                            proyectos.forEach(proyecto => {
                             
                                 template += `
                                     <tr productId="${producto.id}">
                                         <td>${producto.id}</td>
                                         <td><a href="#" class="product-item">${producto.nombre}</a></td>
-                                        <td><ul>${descripcion}</ul></td>
+                                        <td><ul>${proyecto.descripcion}</ul></td>
                                         <td>
-                                            <button class="product-delete btn btn-danger">
+                                            <button class="project-delete btn btn-danger">
                                                 Eliminar
                                             </button>
                                         </td>
                                     </tr>
                                 `;
-
-                                template_bar += `
-                                    <li>${producto.nombre}</il>
-                                `;
                             });
                             // SE HACE VISIBLE LA BARRA DE ESTADO
-                            $('#product-result').show();
-                            // SE INSERTA LA PLANTILLA PARA LA BARRA DE ESTADO
-                            $('#container').html(template_bar);
+                            $('#project-result').show();
                             // SE INSERTA LA PLANTILLA EN EL ELEMENTO CON ID "productos"
-                            $('#products').html(template);    
+                            $('#projects').html(template);    
                         }
                     }
                 }
             });
         }
         else {
-            $('#product-result').hide();
+            $('#project-result').hide();
         }
     });
 
-    $('#product-form').submit(e => {
+    $('#project-form').submit(e => {
         e.preventDefault();
-
-        // SE CONVIERTE EL JSON DE STRING A OBJETO
-        let postData = JSON.parse( $('#description').val() );
         // SE AGREGA AL JSON EL NOMBRE DEL PRODUCTO
         postData['nombre'] = $('#name').val();
         postData['id'] = $('#productId').val();
-
+        postData['descripcion'] = $('#description').val();
         /**
          * AQUÍ DEBES AGREGAR LAS VALIDACIONES DE LOS DATOS EN EL JSON
          * --> EN CASO DE NO HABER ERRORES, SE ENVIAR EL PRODUCTO A AGREGAR
          **/
 
-        const url = edit === false ? './backend/product-add.php' : './backend/product-edit.php';
+        const url = edit === false ? './backend/project-add.php' : './backend/project-edit.php';
         
         $.post(url, postData, (response) => {
             console.log(response);
             // SE OBTIENE EL OBJETO DE DATOS A PARTIR DE UN STRING JSON
             let respuesta = JSON.parse(response);
             // SE CREA UNA PLANTILLA PARA CREAR INFORMACIÓN DE LA BARRA DE ESTADO
-            let template_bar = '';
-            template_bar += `
-                        <li style="list-style: none;">status: ${respuesta.status}</li>
-                        <li style="list-style: none;">message: ${respuesta.message}</li>
-                    `;
+            
             // SE REINICIA EL FORMULARIO
             $('#name').val('');
-            $('#description').val(JsonString);
-            // SE HACE VISIBLE LA BARRA DE ESTADO
-            $('#product-result').show();
-            // SE INSERTA LA PLANTILLA PARA LA BARRA DE ESTADO
-            $('#container').html(template_bar);
+            $('#description').val('');
             // SE LISTAN TODOS LOS PRODUCTOS
-            listarProductos();
+            listarProyectos();
             // SE REGRESA LA BANDERA DE EDICIÓN A false
             edit = false;
         });
     });
 
-    $(document).on('click', '.product-delete', (e) => {
-        if(confirm('¿Realmente deseas eliminar el producto?')) {
+    $(document).on('click', '.project-delete', (e) => {
+        if(confirm('¿Realmente deseas eliminar el proyecto?')) {
             const element = $(this)[0].activeElement.parentElement.parentElement;
-            const id = $(element).attr('productId');
-            $.post('./backend/product-delete.php', {id}, (response) => {
-                $('#product-result').hide();
+            const id = $(element).attr('projectId');
+            $.post('./backend/project-delete.php', {id}, (response) => {
                 listarProductos();
             });
         }
     });
 
-    $(document).on('click', '.product-item', (e) => {
+    $(document).on('click', '.project-item', (e) => {
         const element = $(this)[0].activeElement.parentElement.parentElement;
-        const id = $(element).attr('productId');
-        $.post('./backend/product-single.php', {id}, (response) => {
+        const id = $(element).attr('projectId');
+        $.post('./backend/project-single.php', {id}, (response) => {
             // SE CONVIERTE A OBJETO EL JSON OBTENIDO
-            let product = JSON.parse(response);
+            let project = JSON.parse(response);
             // SE INSERTAN LOS DATOS ESPECIALES EN LOS CAMPOS CORRESPONDIENTES
-            $('#name').val(product.nombre);
+            $('#name').val(project.nombre);
+            $('#description').val(project.descripcion);
             // EL ID SE INSERTA EN UN CAMPO OCULTO PARA USARLO DESPUÉS PARA LA ACTUALIZACIÓN
-            $('#productId').val(product.id);
-            // SE ELIMINA nombre, eliminado E id PARA PODER MOSTRAR EL JSON EN EL <textarea>
-            delete(product.nombre);
-            delete(product.eliminado);
-            delete(product.id);
-            // SE CONVIERTE EL OBJETO JSON EN STRING
-            let JsonString = JSON.stringify(product,null,2);
-            // SE MUESTRA STRING EN EL <textarea>
-            $('#description').val(JsonString);
+            $('#projectId').val(project.id);
             
             // SE PONE LA BANDERA DE EDICIÓN EN true
             edit = true;
